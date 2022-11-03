574ddfc8b01d434a2546c22314c518af2ddd64d8
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-11-02 21:41:56 -0400

    Adding inital chores

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import DailyChoreChart from './view/DailyChoreChart';
+
+function App() {
+  return (
+    <DailyChoreChart/>
+  );
+}
+
+export default App;
---
+++ src/view/DailyChoreChart.tsx
@@ -0,0 +1,16 @@
+
+
+export default function DailyChoreChart() {
+    return (
+      <div>
+        <h1>Maybree Chores!</h1>
+        <div className="chore-item">
+          <img src="makingbed.png" alt="bed"></img>
+        <label>
+          <input type="checkbox" />
+          Make bed
+        </label>
+        </div>
+      </div>
+    );
+  }
