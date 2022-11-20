a1b40dd1b4d4cdfc6e12330016e94aec8e253b54
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-11-19 23:37:48 -0500

    Adding changes to chore: ui

--- src/view/DailyChoreChart.tsx
+++ src/view/DailyChoreChart.tsx
@@ -1,16 +1,32 @@
+import fetch from 'node-fetch';
 
+interface Chore {
+  name: string,
+  description: string,
+  age: number,
+}
 
 export default function DailyChoreChart() {
+
+  const getChores = async () => {
+    const response = await fetch('http://localhost:8081/chores')
+    let data = await response.json()
+    
+    let mappedChores = data.map((chore: Chore)=>{
+      return<li>{chore.name}</li>
+    })
+
+    console.log(mappedChores)
     return (
+      <ul>{mappedChores}</ul>
+    )
+  }
+
+  window.onload = getChores
+    return (
+
       <div>
         <h1>Maybree Chores!</h1>
-        <div className="chore-item">
-          <img src="makingbed.png" alt="bed"></img>
-        <label>
-          <input type="checkbox" />
-          Make bed
-        </label>
-        </div>
       </div>
     );
   }
